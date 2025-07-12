72771edba5d99cef984df51b6fe58600c615a326
Nazar Didych <127863248+didychnazar@users.noreply.github.com>
2025-07-12 12:05:23 +0200

    all

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+/*З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+
+Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+
+
+На що буде звертати увагу ментор при перевірці:
+
+Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
+Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()*/
+
+const listOfCategories = document.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${listOfCategories.length}`);
+
+Array.from(listOfCategories).forEach(category => {
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`Elements: ${category.querySelectorAll('ul li').length}`);
+});
---
+++ js/task-5.js
@@ -0,0 +1,27 @@
+/* Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і задає це значення кольору текстовим вмістом для span.color.
+
+Для генерування випадкового кольору використовуй функцію getRandomHexColor().
+
+Зверни увагу, що функція getRandomHexColor() повертає колір у hex-форматі, в той час, як колір фону на <body> буде у форматі rgb. Це нормально й не потребує якихось правок.
+
+На що буде звертати увагу ментор при перевірці:
+
+Фон на <body> задається тільки після кліку на button.change-color
+При кожному кліку на елемент button.change-color фон <body> зафарбовується новим рандомним кольором
+На <body> і span.color значення одного й того самого кольору   */
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyElement = document.querySelector('body');
+const changeColorBtn = document.querySelector('button.change-color');
+const colorName = document.querySelector('span.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  colorName.innerText = color;
+  bodyElement.style.backgroundColor = color;
+});
